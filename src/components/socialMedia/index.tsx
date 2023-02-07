a6fbc086abe0d34efd5c9fe05a9deaab096ee0c2
PatrickWatson887 <patrick.watson887@gmail.com>
2023-02-07 23:12:59 +0000

    fix error

--- src/components/socialMedia/index.tsx
+++ src/components/socialMedia/index.tsx
@@ -3,22 +3,16 @@ import Link from 'next/link'
 import react from 'react'
 
 const SocialMedia: React.FC = () => {
-  const icons = [
-    require('./../../../public/Facebook.png'),
-    require('./../../../public/Instagram.png'),
-    require('./../../../public/Twitter.png'),
-    require('./../../../public/Soundcloud.jpeg'),
-    require('./../../../public/Youtube.svg'),
-    require('./../../../public/Spotify.png'),
-  ]
 
   return (
     <div className='flex flex-row flex-wrap gap-2'>
-      {
-        icons.map((icon, index) => (
-          <Image src={icon} alt={''} width={48} height={48} key={index} />
-        ))
-      }
+    <Image src={'/Facebook.png'} alt={''} width={48} height={48}></Image>
+    <Image src={'/Instagram.png'} alt={''} width={48} height={48}></Image>
+    <Image src={'/Twitter.png'} alt={''} width={48} height={48}></Image>
+    <Image src={'/Soundcloud.jpeg'} alt={''} width={48} height={48}></Image>
+    <Image src={'/Youtube.svg'} alt={''} width={48} height={48}></Image>
+    <Image src={'/Spotify.png'} alt={''} width={48} height={48}></Image>
+
     </div>
   )
 }
