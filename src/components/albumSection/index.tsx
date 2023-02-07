8a1c5e68e38113d9763c877ac2baa448bc26a483
PatrickWatson887 <patrick.watson887@gmail.com>
2023-02-07 23:15:22 +0000

    Fix width

--- src/components/albumSection/index.tsx
+++ src/components/albumSection/index.tsx
@@ -12,7 +12,7 @@ const AlbumSection: React.FC<AlbumSectionProps> = ({title, description}) => {
       <div className='md:ml-auto md:mx-0 mx-auto'>
         1
       </div>
-      <div className='md:mr-auto md:mx-0 mx-auto w-1/2'>
+      <div className='md:mr-auto md:mx-0 mx-auto md:w-1/2 px-3'>
         <h1 className='text-semi-bold text-xl'>{title}</h1>
         <div className='bg-black h-2 w-16 mt-10 mb-5'></div>
         <text className=''>{description}</text>
